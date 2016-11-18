d232db7bd854125b0b55a5fec2a6e31a3b1d706c
a8256303 <K.A.Garside2@newcastle.ac.uk>
2016-11-18 15:31:55 +0000

    Added code to remove NA's in dataframe

---
+++ scripts-r/exploration.R
@@ -0,0 +1,30 @@
+# Missing data
+
+library(ElemStatLearn)
+?marketing
+data(marketing)
+names(marketing)
+attach(marketing)
+
+# How many NA entries?
+predictors=marketing[-1]
+checkForNA=function(predictors){
+  for(i in 1:ncol(predictors)){
+    na_count=sum(is.na(predictors[,i]))
+    output=paste(names(predictors[i]),":",na_count)
+    print(output)
+  }
+}
+checkForNA(predictors)
+
+# Remove row if there is any missing data 
+marketing_naRemoved<-marketing[complete.cases(marketing),]
+predictors_naRemoved=marketing_naRemoved[-1]
+checkForNA(predictors_naRemoved)
+
+
+# Replace missing values with the mean
+
+sum(is.na(Marital_naRemoved))
+Marital_naRemoved<-Marital[complete.cases(Marital)]
+mean(Marital_naRemoved)
